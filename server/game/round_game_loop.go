f86e437f6b5921cb3517fbd95ae9a927e3bfdb51
jaska <pusanteemu@gmail.com>
2021-06-13 00:43:50 +0300

    Implement some logic to round game loop

--- server/game/round_game_loop.go
+++ server/game/round_game_loop.go
@@ -1,19 +1,30 @@
 package game
 
+import "math"
+
 type RoundUnit struct {
-	id      string
-	unitId  string
-	rank    int
-	maxHP   int
-	maxMana int
-	hp      int
-	mana    int
-	xCoor   int
-	yCoor   int
+	id           string
+	unitId       string
+	rank         int
+	maxHP        int
+	maxMana      int
+	hp           int
+	mana         int
+	xCoor        int
+	yCoor        int
+	attackRange  int
+	attackSpeed  int
+	attackDamage int
+
+	instantAttack bool
+
+	sleep int
+
+	dead bool
 
 	sinceLastAttack int
 
-	currentAttackTarget string
+	currentAttackTarget *RoundUnit
 }
 
 type RoundGameLoop struct {
@@ -27,22 +38,86 @@ func (roundGameLoop *RoundGameLoop) OnFinish() <-chan interface{} {
 
 func (roundGameLoop *RoundGameLoop) Run() {
 	for {
-		roundGameLoop.work()
+		roundGameLoop.work(1)
 	}
 }
 
-func (roundGameLoop *RoundGameLoop) work() {
-	for _, unit := range roundGameLoop.playerOneUnits {
-		if unit.currentAttackTarget == "" {
-			unit := findClosestUnit(unit, roundGameLoop.playerTwoUnits)
+func (roundGameLoop *RoundGameLoop) work(delta float32) {
+	findAttackTargetForUnits(roundGameLoop.playerOneUnits, roundGameLoop.playerTwoUnits)
+	findAttackTargetForUnits(roundGameLoop.playerTwoUnits, roundGameLoop.playerOneUnits)
 
-			unit.currentAttackTarget = unit.id
-		}
-	}
+	handleIsInsideAttackRange(roundGameLoop.playerOneUnits)
+	handleIsInsideAttackRange(roundGameLoop.playerTwoUnits)
+
+	moveUnits(roundGameLoop.playerOneUnits, delta)
+	moveUnits(roundGameLoop.playerTwoUnits, delta)
 
 	// for _, unit := range roundGameLoop.playerTwoUnits {
 
 	// }
+}
+
+func handleIsInsideAttackRange(units map[string]*RoundUnit) {
+	for _, unit := range units {
+		target := unit.currentAttackTarget
+
+		if !isInsideAttackRange(
+			unit.xCoor,
+			unit.yCoor,
+			target.xCoor,
+			target.yCoor,
+			unit.attackRange,
+		) {
+			continue
+		}
+
+		target.hp -= unit.attackDamage
+
+		if target.hp > 0 {
+			continue
+		}
+
+		unit.dead = true
+		delete(units, unit.id)
+	}
+}
+
+func moveUnits(units map[string]*RoundUnit, delta float32) {
+	for _, unit := range units {
+		xDiff := unit.xCoor - unit.currentAttackTarget.xCoor
+		xDist := int(math.Abs(float64(xDiff)))
+
+		yDiff := unit.yCoor - unit.currentAttackTarget.yCoor
+		yDist := int(math.Abs(float64(yDiff)))
+
+		if xDist > yDist {
+			unit.yCoor += int(float32(yDist) * delta)
+		} else {
+			unit.xCoor += int(float32(xDist) * delta)
+		}
+	}
+}
+
+func isInsideAttackRange(yourX int, yourY int, x int, y int, attackRange int) bool {
+	if math.Abs(float64(yourX-x)) <= float64(attackRange) {
+		return true
+	}
+
+	if math.Abs(float64(yourY-y)) <= float64(attackRange) {
+		return true
+	}
+
+	return false
+}
+
+func findAttackTargetForUnits(units map[string]*RoundUnit, enemyUnits map[string]*RoundUnit) {
+	for _, unit := range units {
+		if unit.currentAttackTarget == nil {
+			unit := findClosestUnit(unit, enemyUnits)
+
+			unit.currentAttackTarget = unit
+		}
+	}
 }
 
 func findClosestUnit(
