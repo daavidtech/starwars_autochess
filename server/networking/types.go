976938348656c517c62661f8f6565bd0973f83e3
Jaska <pusanteemu@gmail.com>
2021-06-16 00:12:49 +0300

    Implement some datastructures for different game events

--- server/networking/types.go
+++ server/networking/types.go
@@ -20,8 +20,77 @@ type ChangeUnitPosition struct {
 	Y  int    `json:"y"`
 }
 
+type GamePhaseChanged struct {
+	NewPhaseType string
+}
+
+type UnitBought struct {
+	UnitType string `json:"unitType"`
+}
+
+type UnitUpgraded struct {
+	UnitId string `json:"unitId"`
+	Rank   int    `json:"rank"`
+}
+
+type UnitSold struct {
+	UnitId string `json:"unitId"`
+}
+
+type StartTimerTimeChanged struct {
+	NewTimerValue int `json:"newTimerValue"`
+}
+
+type UnitDied struct {
+	UnitID string `json:"unitId"`
+}
+
+type UnitPlaced struct {
+	UintID   string `json:"unitId"`
+	UnitType string `json:"unitType"`
+	X        int    `json:"x"`
+	Y        int    `json:"y"`
+}
+
+type UnitStartedMovingTo struct {
+	UnitID string `json:"unitId"`
+	X      int    `json:"x"`
+	Y      int    `json:"y"`
+}
+
+type UnitArrivedToPosition struct {
+	UnitID string `json:"unitId"`
+	X      int    `json:"x"`
+	Y      int    `json:"Y"`
+}
+
+type UnitStartedAttacking struct {
+	UnitID string `json:"unitId"`
+}
+
+type UnitStoppedAttacking struct {
+	UnitID string `json:"unitId"`
+}
+
+type LaunchParticle struct {
+	StartPointX  int    `json:"startPointX"`
+	StartPointY  int    `json:"startPointY"`
+	EndPointX    int    `json:"endPointX"`
+	EndPointY    int    `json:"endPointY"`
+	Speed        int    `json:"speed"`
+	ParticleType string `json:"particleType"`
+}
+
 type MessageToClient struct {
-	CreateUnit         *CreateUnit         `json:"createUnit"`
-	RemoveUnit         *RemoveUnit         `json:"removeUnit"`
-	ChangeUnitPosition *ChangeUnitPosition `json:"changeUnitPosition"`
+	GamePhaseChanged      *GamePhaseChanged      `json:"gamePhaseChanged"`
+	UnitBought            *UnitBought            `json:"unitBought"`
+	UnitSold              *UnitSold              `json:"unitSold"`
+	StartTimerTimeChanged *StartTimerTimeChanged `json:"startTimerTimeChanged"`
+	UnitDied              *UnitDied              `json:"unitDied"`
+	UnitPlaced            *UnitPlaced            `json:"unitPlaced"`
+	UnitStartedMovingTo   *UnitStartedMovingTo   `json:"unitStartedMovingTo"`
+	UnitArrivedToPosition *UnitArrivedToPosition `json:"unitArrivedToPosition"`
+	UnitStartedAttacking  *UnitStartedAttacking  `json:"unitStartedAttacking"`
+	UnitStoppedAttacking  *UnitStoppedAttacking  `json:"unitStoppedAttacking"`
+	LaunchParticle        *LaunchParticle        `json:"launchParticle"`
 }
