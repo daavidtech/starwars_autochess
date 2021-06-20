a34958aad0c502b1b3d0286d1abba3f475c3f3f4
Jaska <pusanteemu@gmail.com>
2021-06-21 00:25:55 +0300

    Do some server event wiring

--- scenes/TestNetworking.gd
+++ scenes/TestNetworking.gd
@@ -1,23 +1,35 @@
 extends Node
 
 var GameState = preload("res://scenes/game_state.gd")
-var Unit = preload("res://scenes/unit.tscn")
+var YourUnit = preload("res://scenes/your_unit.tscn")
+
+var ServerConnection = preload("res://scenes/server_connection.gd")
 
 signal unit_choosen
 
 onready var placement_area = $StaticBody/PlacementArea
 onready var unit_shop = $unit_shop
+onready var unit_barrack = $unit_barrack
+onready var your_health = $your_health
+onready var your_level = $your_level
+onready var your_money = $your_money
 
-var unit_map = {}
+var units = {}
+var placement_units = {}
+
+var width = 100
+var heigth = 100
 
 var game_state
+
+var conn
 
 func load_thing(type: String):
 #	var path = "res://assets/exported/"
 #
 #	path += type + "/" + type + ".gltf"
 	
-	var path = "res://assets/unit_droid/unit_droid.glb"
+	var path = "res://scenes/your_unit.tscn"
 	
 	print("Loading resource from ", path)
 	
@@ -29,7 +41,12 @@ func load_thing(type: String):
 	
 	var i = Droid.instance()
 	
+	i.translation.y = placement_area.translation.y
+	
 	add_child(i)
+	
+	i.move_speed = 5;
+	i.start_move(conv_coords(50, 50))
 	
 
 func move_to_point(u, x: int, y: int):
@@ -57,6 +74,9 @@ func move_to_point(u, x: int, y: int):
 	u.translation.y = placement_area.translation.y
 
 func _ready():
+	conn = ServerConnection.new()
+	conn.connect("new_message", self, "_handle_msg")
+	
 	game_state = GameState.new()
 	add_child(game_state)
 	
@@ -89,20 +109,150 @@ func _ready():
 	
 	unit_shop.connect("unit_bought", self, "_handle_unit_bought")
 
+func _handle_msg(msg):
+	if msg.has("gamePhaseChanged"):
+		handle_game_phase_changed(msg.gamePhaseChanged)
+	if msg.has("unitBought"):
+		handle_unit_bought(msg.unitBought)
+	if msg.has("unitSold"):
+		handle_unit_sold(msg.unitSold)
+	if msg.has("unitUpgraded"):
+		handle_unit_upgraded(msg.unitUpgraded)
+	if msg.has("startTimerTimeChanged"):
+		pass
+	if msg.has("unitDied"):
+		handle_unit_died(msg.unitDied)
+	if msg.has("unitPlaced"):
+		handle_unit_placed(msg.unitPlaced)
+	if msg.has("unitTookDamage"):
+		pass
+	if msg.has("unitUsedMana"):
+		pass
+	if msg.has("unitUsedAbility"):
+		pass
+	if msg.has("unitStartedMovingTo"):
+		handle_unit_started_moving(msg.unitStartedMovingTo)
+	if msg.has("unitArrivedToPosition"):
+		handle_unit_arrived_to_position(msg.unitArrivedToPosition)
+	if msg.has("unitStartedAttacking"):
+		handle_unit_started_attacking(msg["unitStartedAttacking"])
+	if msg.has("unitStoppedAttacking"):
+		handle_unit_stopped_attacking(msg["unitStoppedAttacking"])
+	if msg.has("launchParticle"):
+		pass
+	if msg.has("playerMoneyChanged"):
+		handle_player_money_changed(msg["playerMoneyChanged"])
+	if msg.has("playerLevelChanged"):
+		handle_player_level_changed(msg["playerLevelChanged"])
+	if msg.has("playerHealthChanged"):
+		handle_player_health_changed(msg["playerHealthChanged"])
+
+func handle_unit_started_moving(unit_started_moving):
+	if units.has(unit_started_moving.unitId):
+		var unit = units[unit_started_moving.unitId]
+		
+		unit.start_moving(conv_coords(unit_started_moving.x, unit_started_moving.y))
+	
+func handle_unit_arrived_to_position(unit_arrived):
+	if units.has(unit_arrived.unitId):
+		var unit = units[unit_arrived.unitId]
+		
+		unit.move_to_position(conv_coords(unit_arrived.x, unit_arrived.y))
+
+func handle_unit_started_attacking(unit_started_attacking):
+	if units.has(unit_started_attacking.unitId):
+		var unit = units[unit_started_attacking.unitId]
+		
+		unit.attacking = true
+
+func handle_unit_stopped_attacking(unit_stopped_attacking):
+	if units.has(unit_stopped_attacking.unit):
+		var unit = units[unit_stopped_attacking.unit]
+		
+		unit.attacking = false
+
+func handle_player_money_changed(your_money_changed):
+	your_money.text = String(your_money_changed.newMoney)
+
+func handle_player_level_changed(player_level_changed):
+	your_level.text = String(player_level_changed.newLevel)
+
+func handle_player_health_changed(player_health_changed):
+	your_health.text = String(player_health_changed.newHp)
+
+func handle_game_phase_changed(game_phase_changed):
+	match game_phase_changed:
+		"BuyingPhase":
+			unit_shop.visible = true
+		"PlacementPhase":
+			unit_shop.visible = false
+		"BattlePhase":
+			unit_shop.visible = false
+
+func handle_unit_bought(unit_bought):
+	var new_unit = YourUnit.instance()
+	
+	new_unit.unit_id = unit_bought.unit_id
+	new_unit.unit_type 
+	new_unit.hp = unit_bought.hp
+	new_unit.mana = unit_bought.mana
+	new_unit.attack_rate = unit_bought.attackRate
+	
+	units[unit_bought.unit_id] = new_unit
+	unit_barrack.add_unit(new_unit)
+	
+func handle_unit_upgraded(unit_upgraded):
+	var unit = units[unit_upgraded.unit_id]
+	
+	unit.hp = unit_upgraded.hp
+	unit.mana = unit_upgraded.mana
+	unit.attack_rate = unit_upgraded.attack_rate
+	unit.rank = unit_upgraded.rank
+	
+func handle_unit_sold(unit_sold):
+	if units.has(unit_sold.unitId):
+		var unit = units[unit_sold.unitId]
+		
+		unit_barrack.remove_unit(unit)
+	
+func handle_unit_died(unit_died):
+	if units.has(unit_died.unitId):
+		var unit = units[unit_died.unitId]
+		
+		placement_area.remove_child(unit)
+	
+func handle_unit_placed(unit_placed):
+	pass
+
+
 func _handle_unit_bought(unit):
 	print("unit bought ", unit)
 
-func _handle_create_unit(id, unit_type, x, y):
-	print("handle create unit")
+func conv_coords(x: int, y: int) -> Vector3:
+	var size = placement_area.shape.extents
+	
+	var start_x = -size.x
+	var start_y = -size.z
+	
+	var x_ratio = size.z / width
+	var y_ratio = size.x / heigth
+	
+	var convz = start_x + x_ratio * x
+	var convx = start_y + y_ratio * y
+	
+	return Vector3(convx, placement_area.translation.y, convz)
+	
+#func _handle_create_unit(id, unit_type, x, y):
+#	print("handle create unit")
+#
+#	var new_unit = Unit.instance()
+#
+#	move_to_point(new_unit, x, y)
+#
+#	placement_area.add_child(new_unit)
+#	unit_map[id] = new_unit
 
-	var new_unit = Unit.instance()
-	
-	move_to_point(new_unit, x, y)
-	
-	placement_area.add_child(new_unit)
-	unit_map[id] = new_unit
-
-func _handle_change_unit_position(id, x: int, y: int):
-	var unit = unit_map.get(id)
-	
-	move_to_point(unit, x, y)
+#func _handle_change_unit_position(id, x: int, y: int):
+#	var unit = unit_map.get(id)
+#
+#	move_to_point(unit, x, y)
