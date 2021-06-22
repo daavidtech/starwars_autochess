4cf57facfbb8acd7804490db7a2ae2430e378e8a
Jaska <pusanteemu@gmail.com>
2021-06-23 00:59:47 +0300

    Implment constructor method for TierProbabilities

--- server/game/tier-probabilities.go
+++ server/game/tier-probabilities.go
@@ -1,8 +1,16 @@
 package game
 
 type TierProbabilities struct {
+	probabilities [][]int
+}
+
+func NewTierProbabilities(probabilities [][]int) TierProbabilities {
+	return TierProbabilities{
+		probabilities: probabilities,
+	}
 }
 
 func (t *TierProbabilities) PickLevel(level int) []int {
+
 	return []int{}
 }
