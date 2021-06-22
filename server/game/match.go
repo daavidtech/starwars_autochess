b1a3bee9e487cf50a7e39bbaf198d64c183cc4a5
Jaska <pusanteemu@gmail.com>
2021-06-23 00:59:18 +0300

    Add some fields for Match struct

--- server/game/match.go
+++ server/game/match.go
@@ -1,7 +1,28 @@
 package game
 
+type MatchPhase string
+
+const (
+	Lobby          MatchPhase = "Lobby"
+	ShoppingPhase  MatchPhase = "ShoppingPhase"
+	PlacementPhase MatchPhase = "PlacementPhase"
+	BattlePhase    MatchPhase = "BattlePhase"
+)
+
 type Match struct {
 	id                 string
 	currentRoundNumber int
+	phase              MatchPhase
 	players            []Player
+
+	shop              Shop
+	TierProbabilities TierProbabilities
+}
+
+func NewMatch() Match {
+	return Match{}
+}
+
+func (match *Match) start() {
+
 }
