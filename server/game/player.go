4c42b08a5cab0e90cfedce25c5f4b73c976833c8
Jaska <pusanteemu@gmail.com>
2021-06-23 00:58:48 +0300

    Implement some functionality for player and tests

--- server/game/player.go
+++ server/game/player.go
@@ -1,15 +1,71 @@
 package game
 
-type Placement struct {
-	x    int
-	y    int
-	unit Unit
+type Player struct {
+	id          string
+	credits     int
+	health      int
+	accepted    bool
+	battleUnits []BattleUnit
 }
 
-type Player struct {
-	id         string
-	credits    int
-	health     int
-	placements []Placement
-	barrack    []Unit
+func (player *Player) AddShopUnit(shopUnit ShopUnit) {
+	count := player.countUnitType(shopUnit.UnitType, 1)
+
+	if count != 2 {
+		player.battleUnits = append(player.battleUnits, BattleUnit{
+			unitId:     shopUnit.UnitID,
+			unitType:   shopUnit.UnitType,
+			tier:       shopUnit.Tier,
+			rank:       1,
+			hp:         shopUnit.HP,
+			mana:       shopUnit.Mana,
+			attackRate: shopUnit.AttackRate,
+		})
+
+		return
+	}
+
+	newBattleUnits := []BattleUnit{}
+
+	removedCount := 0
+
+	for _, unit := range player.battleUnits {
+		if unit.unitType == shopUnit.UnitType || unit.rank == 1 || removedCount < 2 {
+			removedCount += 1
+
+			continue
+		}
+
+		newBattleUnits = append(newBattleUnits, unit)
+	}
+
+	newBattleUnits = append(newBattleUnits, BattleUnit{
+		unitId:     shopUnit.UnitID,
+		unitType:   shopUnit.UnitType,
+		tier:       shopUnit.Tier,
+		rank:       2,
+		hp:         shopUnit.HP,
+		mana:       shopUnit.Mana,
+		attackRate: shopUnit.AttackRate,
+	})
+
+	player.battleUnits = newBattleUnits
+}
+
+func (player *Player) countUnitType(unitType string, rank int) int {
+	count := 0
+
+	for _, battleUnit := range player.battleUnits {
+		if battleUnit.unitType != unitType || battleUnit.rank != rank {
+			continue
+		}
+
+		count += 1
+	}
+
+	return count
+}
+
+func (player *Player) getBattleUnits() []BattleUnit {
+	return player.battleUnits
 }
---
+++ server/game/player_test.go
@@ -0,0 +1,48 @@
+package game
+
+import "testing"
+
+func TestAddsNewShopUnitWhenNoOthers(t *testing.T) {
+	player := Player{}
+
+	player.AddShopUnit(ShopUnit{
+		UnitType: "unit_droid",
+	})
+
+	battleUnits := player.getBattleUnits()
+
+	if len(battleUnits) != 1 {
+		t.Errorf("Invalid number of battle units")
+	}
+}
+
+func TestUpgradeUnit(t *testing.T) {
+	player := Player{
+		battleUnits: []BattleUnit{
+			BattleUnit{
+				unitType: "unit_droid",
+				rank:     1,
+			},
+			BattleUnit{
+				unitType: "unit_droid",
+				rank:     1,
+			},
+		},
+	}
+
+	player.AddShopUnit(ShopUnit{
+		UnitType: "unit_droid",
+	})
+
+	units := player.getBattleUnits()
+
+	if len(units) != 1 {
+		t.Errorf("Invalid number of units")
+	}
+
+	unit := units[0]
+
+	if unit.rank != 2 {
+		t.Errorf("Unit did not get upgraded correctly")
+	}
+}
